7e4a5a1c5ee58005e051acd428bf322de118c7be
alessiamore <alessiamore@proton.me>
2026-07-14 19:57:29 +0300

    Add demo_data.py with sample transactions

---
+++ demo_data.py
@@ -0,0 +1,35 @@
+from transaction import Transaction
+
+EXPENSES = [
+
+    Transaction(
+        "Coffee",
+        "Food",
+        4.50
+    ),
+
+    Transaction(
+        "Internet",
+        "Utilities",
+        35
+    ),
+
+    Transaction(
+        "Book",
+        "Education",
+        18
+    ),
+
+    Transaction(
+        "Bus Ticket",
+        "Transport",
+        3.20
+    ),
+
+    Transaction(
+        "Movie",
+        "Entertainment",
+        12
+    )
+
+]
