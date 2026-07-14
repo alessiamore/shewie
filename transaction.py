9183186a8f4e5ed057f32af952b84606c179026b
alessiamore <alessiamore@proton.me>
2026-07-14 19:56:38 +0300

    Add Transaction class for handling transactions

---
+++ transaction.py
@@ -0,0 +1,12 @@
+class Transaction:
+
+    def __init__(
+        self,
+        title,
+        category,
+        amount
+    ):
+
+        self.title = title
+        self.category = category
+        self.amount = amount
